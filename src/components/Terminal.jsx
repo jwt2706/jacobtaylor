4254ed2caddedacde08004fa72ca95e70ec7e7e7
jwt2706 <jwt2home@gmail.com>
2024-02-24 22:23:16 -0500

    fix invisible terminal bug

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -11,20 +11,50 @@ const commands = [
 
 function Terminal() {
   const [currentCommandIndex, setCurrentCommandIndex] = useState(0);
+  const [currentCommand, setCurrentCommand] = useState("");
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentCommandIndex((prevIndex) => prevIndex + 1);
-    }, 1000); // Change command every 1 second
+    let commandInterval;
+    let typingInterval;
 
-    return () => clearInterval(interval); // Clean up on unmount
-  }, []);
+    typingInterval = setInterval(() => {
+      if (currentCommand.length < commands[currentCommandIndex].length) {
+        setCurrentCommand(
+          commands[currentCommandIndex].substr(0, currentCommand.length + 1)
+        );
+      } else {
+        clearInterval(typingInterval);
+
+        commandInterval = setInterval(() => {
+          setCurrentCommandIndex((prevIndex) => {
+            const newIndex = prevIndex + 1;
+            if (newIndex < commands.length) {
+              setCurrentCommand("");
+              return newIndex;
+            } else {
+              clearInterval(commandInterval);
+              return prevIndex;
+            }
+          });
+        }, 1000);
+      }
+    }, 100); // Type each letter every 100ms
+
+    return () => {
+      clearInterval(typingInterval);
+      clearInterval(commandInterval);
+    }; // Clean up on unmount
+  }, [currentCommand, currentCommandIndex]);
 
   return (
     <div className="terminal">
       <div className="input">
         <span className="prompt">root@jwt2706.ca:~$ </span>
-        <span className="command">{commands[currentCommandIndex]}</span>
+        <span className="command">
+          {currentCommand.length < commands[currentCommandIndex].length
+            ? currentCommand + "_"
+            : currentCommand}
+        </span>
       </div>
     </div>
   );
