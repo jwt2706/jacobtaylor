6213ae48fa47ffc9d90fee056f2a5a630df24c31
jwt2706 <jwt2home@gmail.com>
2024-02-27 09:10:22 -0500

    robots.txt, structured-data, incl tailwinds

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AnimatedCursor from "react-animated-cursor";
 import favicon from "./assets/icons/planet-512.png";
 import image1 from "./assets/images/a.jpg";
 import "./assets/styles/app.scss";
+import "./assets/styles/tailwind.css";
 
 function App() {
   const [terminals, setTerminals] = useState([]);
@@ -69,6 +70,9 @@ function App() {
           <div className="block terminal-button">
             <button onClick={createTerminal}>
               Open terminal ({terminals.length})
+            </button>
+            <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+              Shimmer
             </button>
           </div>
         </div>
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   build: {
     outDir: 'dist', // Output directory for the built files
+    assetsDir: '.', // Assets directory for the built files
     minify: true, // Enable minification for production build
     sourcemap: false, // Disable sourcemaps for production build
   },
