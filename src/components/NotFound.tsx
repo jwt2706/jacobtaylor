b57ae805c36f3d24c9a8b1da24cdcefa3bed45b4
jwt2706 <jwt2home@gmail.com>
2024-04-04 20:10:42 -0400

    404 redirections

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const NotFound: FC = () => {
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>Redirecting you to the home page...</p>
+    </div>
+  );
+}
+
+export default NotFound;
