d2b061c9cfe950a0435f8cbb794c57120f90e9b6
jwt2706 <jwt2home@gmail.com>
2024-09-07 15:23:47 -0400

    added the dock + cleaned up some stuff

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,36 @@
 import React, { useState } from "react";
 import Vortex from "./Vortex";
-import Sidebar from "./Sidebar";
-import AnimatedCursor from "react-animated-cursor";
+import FloatingDock from './Dock';
 import { IoPlanet } from "react-icons/io5";
-import { LinksCard } from "./LinksCard";
-import { LuCopyright } from "react-icons/lu";
-import { FiExternalLink } from 'react-icons/fi';
+import { FaGithub, FaEnvelope } from "react-icons/fa";
+import { SiGnuprivacyguard } from "react-icons/si";
+import { SiHuggingface } from "react-icons/si";
 import "../assets/styles/app.scss";
 import "../assets/styles/tailwind.css";
+
+const dockitems = [
+  {
+    title: "Email Me!",
+    icon: <FaEnvelope size={20} />,
+    href: "mailto:hey@jwt2706.ca"
+  },
+  {
+    title: "Checkout my open-source projects!",
+    icon: <FaGithub size={20} />,
+    href: "https://github.com/jwt2706"
+  },
+  {
+    title: "Checkout my open-source AI models!",
+    icon: <SiHuggingface size={20} />,
+    href: "https://huggingface.co/jwt2706"
+  },
+  {
+    title: "Download my public GPG key!",
+    icon: <SiGnuprivacyguard size={20} />,
+    href: "https://jwt2706.github.io/jwt2706_public.key"
+  }
+]
+
 
 const Home: React.FC = () => {
   const isTouchDevice = useState<boolean>('ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.maxTouchPoints > 0);
@@ -15,31 +38,8 @@ const Home: React.FC = () => {
   return (
     <div className="animate-fade-in">
       <div className="app flex flex-col min-h-screen text-white">
-        <Sidebar />
         <main className="scrollable flex-grow overflow-y-auto">
-          {!isTouchDevice && (
-            <AnimatedCursor
-              innerSize={20}
-              outerSize={16}
-              color="265, 265, 265"
-              outerAlpha={0.2}
-              innerScale={0.7}
-              outerScale={5}
-              clickables={[
-                "a",
-                'input[type="text"]',
-                'input[type="email"]',
-                'input[type="number"]',
-                'input[type="submit"]',
-                'input[type="image"]',
-                "label[for]",
-                "select",
-                "textarea",
-                "button",
-                ".link",
-              ]}
-            />
-          )}
+ 
           <div>
             <br />
             <div className="mx-auto inline-block p-4">
@@ -60,18 +60,10 @@ const Home: React.FC = () => {
               Check out some of my projects on the sidebar!
             </code>
           </div>
-          <LinksCard />
-          <a href="/jwt2706_public.key" target="_blank" rel="noopener noreferrer" aria-label="GPG key" className="inline-flex items-center px-2 text-gray-400 text-lg bg-neutral-800 p-1 rounded mb-4">
-            GPG key
-            <FiExternalLink className="ml-2" aria-hidden="true" />
-          </a>
+          <div className="flex items-center justify-center h-[35rem] w-full">
+            <FloatingDock items={dockitems} />
+          </div>
         </main>
-        <footer>
-          <code>
-            Made with &lt;3 | <LuCopyright style={{ display: "inline" }} />{" "}
-            jwt2706 {new Date().getFullYear()}
-          </code>
-        </footer>
         <Vortex particleCount={400} baseSpeed={0.0} rangeSpeed={0.1} baseRadius={1.8} rangeRadius={2.5} rangeY={500} baseHue={180}/>
       </div>
     </div>
