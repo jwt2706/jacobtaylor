3c2544c871a0edf54474180417a181d5b23b12c7
jwt2706 <jwt2home@gmail.com>
2024-12-24 15:34:31 -0500

    fix some formatting and stuff

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,8 +15,6 @@ const Footer: React.FC = () => {
                 <p>&copy; {new Date().getFullYear()} jwt2706. All rights reserved.</p>
                 <p>
                     <button onClick={scrollToTop} className="text-green-500 hover:underline cursor-pointer bg-transparent border-none p-0">Back to top</button>
-                    <span> | </span>
-                    <a href="mailto:hey@jwt2706.ca" className="text-green-500 hover:underline">Contact</a>
                 </p>
             </div>
         </footer>
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 
 const Intro: React.FC = () => {
-
     return (
         <div className="text-left">
             <h3 className="text-2xl font-bold mb-4">
                 Hey!
             </h3>
-            <p className="text-justify mt-4 mb-8">
-                I'm <b>jwt2706</b>, I'm from 🇨🇦 and I like building stuff. I've been programming for 10+ years with projects ranging from webdev, to robotics, to AI/ML, and more recently, basic OS dev. You can check out some of my public project below!
-                <br />
-                <br />
-                If you wanna chat, feel free to reach out at <a href="mailto:hey@jwt2706.ca" className="underline bg-slate-950 rounded-md px-1 py-1">hey@jwt2706.ca</a>. :D
-            </p>
+            <div className="bg-gray-800 bg-opacity-75 p-4 rounded-md">
+                <p className="text-justify">
+                    I'm <b>jwt2706</b>, I'm from 🇨🇦 and I like building stuff. I've been programming for 10+ years with projects ranging from webdev, to robotics, to AI/ML, and more recently, basic OS dev. You can check out some of my public project below!
+                    <br />
+                    <br />
+                    If you wanna chat, feel free to reach out at <a href="mailto:hey@jwt2706.ca" className="underline bg-slate-950 rounded-md px-1 py-1">hey@jwt2706.ca</a>. :D
+                </p>
+            </div>
         </div>
     );
 };
